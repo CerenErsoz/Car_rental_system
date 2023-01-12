a17d6ecb7733ca64a6ca7d4f33423756c3fa3f56
CerenErsoz <ersozceren2@gmail.com>
2023-01-13 01:02:58 +0300

    car_rental_system
    car rental system(object oriented programming)

---
+++ car_rental_system/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		Company company = new Company(100, 100, 100, 100);//office, customer ve request sayýsý tutar
+        company.dongu(true);
+	}
+}
